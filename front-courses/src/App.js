ff7d12bbbc2009e425de079ba28e0073801f57cf
VladisIove <vlad.shelemakha0302@gmail.com>
2020-04-16 15:46:05 +0300

    Add react

--- front-courses/src/App.js
+++ front-courses/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import { Home } from './containers/Home/Home'
-import {NavBar} from './containers/NavBar/NavBar'
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <NavBar/>
-      <Switch>
-        <Route path="/" exact>
-          <Home/>
-        </Route>
-        
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
